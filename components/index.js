8b358705d490c69080b4a9c8a57eb4726f02d44d
Vinayak Gupta <75876908+vinayak700@users.noreply.github.com>
2023-03-16 09:39:37 +0530

    1

--- components/index.js
+++ components/index.js
@@ -1,4 +1,4 @@
-export { default as Footer } from './footer'
+export { default as Footer } from './Footer'
 export { default as Layout } from './Layout'
 export { default as Cart } from './Cart'
 export { default as Navbar } from './Navbar'
